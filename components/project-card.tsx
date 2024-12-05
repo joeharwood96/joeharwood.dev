49db1b92f0c0a052ff75909f177fc9f16c6478ce
Joe Harwood <joeharwood3@gmail.com>
2024-12-05 17:39:04 +0100

    fix flex issue

--- components/project-card.tsx
+++ components/project-card.tsx
@@ -14,7 +14,7 @@ export default function ProjectCard({ project }: { project: Project }) {
       <div className="flex flex-col gap-4 p-4">
         <h3 className="text-2xl font-bold">{project.title}</h3>
         <p>{project.description}</p>
-        <div className="flex gap-2">
+        <div className="flex flex-wrap gap-2">
           {project.tags.map((tag) => (
             <p key={tag} className="bg-[#FFC497] rounded-lg px-2 py-1">
               {tag}
